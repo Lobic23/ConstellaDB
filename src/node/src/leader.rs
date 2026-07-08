5f7f476d574bc287b1a6e7f20b3c657f1eed6ea8
fy-st0rm <slokpradhan2062@gmail.com>
2026-07-09 02:30:40 +0545

    Choose the random follower when inserting

--- src/node/src/leader.rs
+++ src/node/src/leader.rs
@@ -3,12 +3,14 @@
 use tokio::net::TcpStream;
 use std::sync::Arc;
 use tokio::sync::Mutex;
+use rand::seq::IteratorRandom;
 
 use protocol_module::{
   handler::{ReadHandler, WriteHandler},
   message::{Message, MessageType},
   serializer::BincodeSerializer,
 };
+use cmd_module::Command;
 
 use crate::node::Node;
 use crate::instruction::NodeStatus;
@@ -36,16 +38,40 @@ pub async fn distribute_message(msg: &Message, node: Arc<Mutex<Node>>) {
     );
 
   if let Some(instruction) = instructions.get(&msg.id) {
-    for (ip, (_, write_handler)) in &instruction.followers {
-      let mut handler = write_handler.lock().await;
-      handler.send(&new_msg).await.unwrap();
+    match new_msg.command.as_ref().unwrap() {
+      Command::Insert(_) => {
+        let chosen = {
+          let mut rng = rand::rng();
 
-      println!("[LOG] Sent to {}:\n{:#?}", &ip, &new_msg);
+          instruction
+           .followers
+            .iter()
+            .choose(&mut rng)
+            .map(|(ip, (_, writer))| {
+              (ip.clone(), writer.clone())
+            })
+        };
+
+        if let Some((ip, write_handler)) = chosen {
+          // Remove non selected followers from instruction's status
+          {
+            let mut status = instruction.nodes_status.lock().await;
+            status.retain(|node| node.id == ip);
+          }
+
+          let mut handler = write_handler.lock().await;
+          handler.send(&new_msg).await.unwrap();
+
+          println!("[LOG] Sent INSERT to {}:\n{:#?}", ip, new_msg);
+        }
+      }
+      _ => {
+        for (ip, (_, write_handler)) in &instruction.followers {
+          let mut handler = write_handler.lock().await;
+          handler.send(&new_msg).await.unwrap();
+          println!("[LOG] Sent to {}:\n{:#?}", ip, new_msg);
+        }
+      }
     }
-  }
-
-  if let Some(instructions) = instructions.get(&msg.id) {
-    let s = instructions.nodes_status.lock().await;
-    println!("[LOG] Instruction {} distributed to: {:?}", msg.id, s);
   }
 }
